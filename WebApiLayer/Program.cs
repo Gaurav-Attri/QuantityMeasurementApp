31a24ff82d0b6883feb9eb32353451596f02d954
Gaurav-Attri <attrigaurav60@gmail.com>
2026-04-01 05:19:29 +0530

    [Gaurav Chaudhary] Refactor: Refactored UC18 to allow the deployment of the backend and the database

--- WebApiLayer/Program.cs
+++ WebApiLayer/Program.cs
@@ -10,7 +10,8 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 builder.Services.AddScoped<IQuantityMeasurementService, QuantityMeasurementService>();
@@ -89,10 +90,15 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseCors("AllowFrontend");
-app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
